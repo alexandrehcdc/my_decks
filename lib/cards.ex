305175c94264db8741ad948e4cb666af19742711
Alexandre Henrique <alexandrehcdc@gmail.com>
2019-04-18 23:59:29 -0300

    Added minor functionalities; Added functions, added ExDoc to the project and added minor tests about Cards module functions

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,37 +1,89 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a conventional deck of cards.
+  """
   
+  @doc """
+    Returns a list of strings representing a deck of cards.
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
-    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    suits  = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits, value <- values do 
+    for suit <- suits, value <- values do
       "#{value} of #{suit}"
-    end 
+    end
   end
 
-  def save deck, filename do
-    File.write filename, :erlang.term_to_binary deck
+  @doc """
+    Saves a deck in a file with the given filename.
+  """
+  def save(deck, filename) do
+    File.write(filename, :erlang.term_to_binary(deck))
   end
 
-  def load filename do
+  @doc """
+    Loads a deck with the given filename.
+  """
+  def load(filename) do
 
-    case File.read filename do
-      {:ok, binary} -> :erlang.binary_to_term binary
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _err} -> "That file could not be read."
     end
     
   end
 
-  def shuffle deck do
-    Enum.shuffle deck
+  @doc """
+    Shuffles the given deck.
+
+  ## Example
+      iex> deck = Cards.create_deck
+      iex> deck != Cards.shuffle(deck)
+      :true
+  """
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 
-  def contains? deck, hand do
-    Enum.member? deck, hand
+  @doc """
+    Checks if the given hand is contained inside the deck.
+
+  ## Example
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> Cards.contains?(deck, hand)
+      :false
+  """
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
   end
 
-  def deal deck, hand_size do
-    Enum.split deck, hand_size
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Example 
+
+      iex> deck = Cards.create_deck
+      iex> {hand, _deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
+  
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  @doc """
+    Creates a new deck, shuffles it and gives a hand based on the given hand size. 
+  """
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,39 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    assert length(Cards.create_deck) == 20
   end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "dealing should return a hand and the remaning deck" do
+    deck = Cards.create_deck
+    {hand,remaningDeck} = Cards.deal(deck, 5)
+    assert length(hand) == 5
+    assert length(remaningDeck) == 15
+  end
+
+  test "contains? should return true if card is contained inside deck" do
+    deck = Cards.create_deck
+    {hand, _remaningDeck} = Cards.deal(deck, 1)
+    assert Cards.contains?(deck, Enum.at(hand, 0)) == :true
+  end
+
+  test "load file should return an error string in case there's no file" do
+    assert Cards.load("inexistent_file") == "That file could not be read."
+  end
+
+  test "load file should return content if there is a existent file" do
+    refute Enum.empty?(Cards.load("my_deck"))
+  end
+
+  test "create_hand should return a list with the given size" do
+    {hand, _remaningDeck} = Cards.create_hand(5)
+    assert length(hand) == 5
+  end
+
 end
